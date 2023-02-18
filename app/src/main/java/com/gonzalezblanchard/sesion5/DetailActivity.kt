00f751ad6f3389b43b7af77580c33dda03f12e73
David Gonzalez <daviddagb2@gmail.com>
2023-02-18 16:49:35 -0600

    Agregue pantalla de detalle

--- app/src/main/java/com/gonzalezblanchard/sesion5/DetailActivity.kt
+++ app/src/main/java/com/gonzalezblanchard/sesion5/DetailActivity.kt
@@ -2,10 +2,20 @@ package com.gonzalezblanchard.sesion5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.gonzalezblanchard.sesion5.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun cerrarPantalla(view: View){
+        finish()
     }
 }
--- app/src/main/java/com/gonzalezblanchard/sesion5/MainActivity.kt
+++ app/src/main/java/com/gonzalezblanchard/sesion5/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.gonzalezblanchard.sesion5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.gonzalezblanchard.sesion5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
+    fun cambiarPantalla(view: View){
+
+        val intent = Intent(this, DetailActivity::class.java);
+        startActivity(intent)
+
+
+        /*val intent = Intent(this, CreateTicketActivity::class.java).apply {
+            //putExtra(EXTRA_MESSAGE, message)
+        }*/
 
     }
+
 }
