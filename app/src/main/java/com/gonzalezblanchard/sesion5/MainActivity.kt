870110390853c6b2df8e887fd1a5cd925e92fa06
David Gonzalez <daviddagb2@gmail.com>
2023-02-27 10:39:16 -0600

    Ajuste a binding

--- app/src/main/java/com/gonzalezblanchard/sesion5/MainActivity.kt
+++ app/src/main/java/com/gonzalezblanchard/sesion5/MainActivity.kt
@@ -12,8 +12,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            button.text = "Cambiar pantalla"
+        }
     }
 
     fun cambiarPantalla(view: View){
